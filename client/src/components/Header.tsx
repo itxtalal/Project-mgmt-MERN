9366f8d5897e773e10f8affa1249622225d05694
Talal <talalstudent@gmail.com>
2022-07-17 20:42:42 +0500

    client vite init

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "../assets/react.svg";
+type Props = {};
+
+const Header = (props: Props) => {
+  return (
+    <nav className="navbar bg-light mb-4 p-0">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          <div className="d-flex">
+            <img src={logo} alt="logo" className="mr-2" />
+          </div>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
